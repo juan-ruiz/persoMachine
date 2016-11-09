06f7abaf12ffe23ad3e7df4c9ca05024e3284702
RUIZ VILLA Juan Sebastian <ruizvilla@viaccess.fr>
2016-11-09 18:22:56 +0100

    puts the ews logic on the main process, frees up the renderer process

--- app/js/init.js
+++ app/js/init.js
@@ -1,62 +1,7 @@
-var EWS = require('node-ews');
+'use-strict'
+const {ipcRenderer} = require('electron');
 
-// exchange server connection info
-var username = 'ruizvilla';
-var password = 'Password2014';
-var host = 'https://mobi.purpledrm.com/';
-
-// disable ssl verification
-var options = {
-//  rejectUnauthorized: false,
-//  strictSSL: false
-};
-
-// initialize node-ews
-var ews = new EWS(username, password, host, options);
-
-// Get Mails
-var ewsFunction = 'FindItem';
-var ewsArgs = {
-    "attributes": {
-    "Traversal": "Shallow"
-  },
-  "ItemShape": {
-    "BaseShape": "IdOnly",
-    "AdditionalProperties": {
-      "FieldURI": {
-        "attributes": {
-          "FieldURI": "item:Subject"
-        }
-      }
-    }
-  },
-  "ParentFolderIds": {
-    "DistinguishedFolderId": {
-      "attributes": {
-        "Id": "inbox"
-      }
-    }
-  },
-  "tns:QueryString": "subject:perso"
-};
-
-//Adding the necessary header to the soap request
-
-var ewsSoapHeader = {
-  't:RequestServerVersion': {
-    attributes: {
-      Version: "Exchange2010"
-    }
-  }
-};
-
-// query ews, print resulting JSON to console
-ews.run(ewsFunction, ewsArgs, ewsSoapHeader)
-  .then(result => {
-    var container = document.getElementById("cont");
-    container.innerHTML=JSON.stringify(result);
-  })
-  .catch(err => {
-    var container = document.getElementById("cont");
-    container.innerHTML=JSON.stringify(err.message);
-  });
+ipcRenderer.on('csat-loaded', (event, message) => {
+  var container = document.getElementById("cont");
+  container.innerHTML=JSON.stringify(message);
+});
--- main.js
+++ main.js
@@ -1,18 +1,76 @@
 'use strict';
-const electron = require('electron')
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
-var mainWindow = null;
+const {BrowserWindow, app} = require('electron');
 
+
+//Microsoft exchange communication initialization
+const EWS = require('node-ews');
+const username = 'ruizvilla';
+const password = 'Password2014';
+const host = 'https://mobi.purpledrm.com/';
+const options = {};
+const ews = new EWS(username, password, host, options);
+const ewsFunction = 'FindItem';
+var ewsArgs = {
+    "attributes": {
+    "Traversal": "Shallow"
+  },
+  "ItemShape": {
+    "BaseShape": "IdOnly",
+    "AdditionalProperties": {
+      "FieldURI": {
+        "attributes": {
+          "FieldURI": "item:Subject"
+        }
+      }
+    }
+  },
+  "ParentFolderIds": {
+    "DistinguishedFolderId": {
+      "attributes": {
+        "Id": "inbox"
+      }
+    }
+  },
+  'tns:QueryString':'Subject:DemandePersoCSAT_201611'
+};
+
+//Adding the necessary header to the soap request
+
+var ewsSoapHeader = {
+  't:RequestServerVersion': {
+    attributes: {
+      Version: "Exchange2010"
+    }
+  }
+};
+
+//Window creation
+let mainWindow;
 app.on('ready', function() {
-    mainWindow = new BrowserWindow({
+        mainWindow = new BrowserWindow({
         height: 600,
         width: 800
     });
 
     mainWindow.loadURL('file://' + __dirname + '/app/index.html');
+
+    // Run a "find item" function to retrieve all CSAT perso requests
+    ews.run(ewsFunction, ewsArgs, ewsSoapHeader)
+      .then(result => {
+       mainWindow.webContents.send('csat-loaded',result);
+      })
+      .catch(err => {
+       mainWindow.webContents.send('csat-loaded',err.message);
+      });
 });
 
+
+
+
+
+
+
+//closes the app when the window is closed
 app.on('window-all-closed', function() {
     if (process.platform != 'darwin') {
         app.quit();
